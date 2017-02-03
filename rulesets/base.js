d96a9dc723fb445861713147245131c3f3ccf913
Danny Andrews <dannya@spiceworks.com>
2017-02-03 16:38:53 -0600

    Add no-invalid-this in

--- rulesets/base.js
+++ rulesets/base.js
@@ -43,6 +43,7 @@ module.exports = {
     'no-implicit-globals': 'error',
     'no-inline-comments': 'error',
     'no-inner-declarations': ['error', 'both'],
+    'no-invalid-this': 'off',
     'no-lonely-if': 'error',
     'no-magic-numbers': [
       'error',
